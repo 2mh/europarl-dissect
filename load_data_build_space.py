e59f76dc13498f92c85a3bfd0cd94c525fbd6a4d
Hernani Marques Madeira <hernani.marquesmadeira@uzh.ch>
2014-06-02 20:03:41 +0200

    Unify paths

--- load_data_build_space.py
+++ load_data_build_space.py
@@ -1,12 +1,13 @@
-
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
 
 from composes.semantic_space.space import Space
 from composes.utils import io_utils
 
 #create a space from co-occurrence counts in sparse format
-my_space = Space.build(data = "./europarl.sm",
-                       rows = "./europarl.row",
-                       cols = "./europarl.row",
+my_space = Space.build(data = "./data/out/europarl.sm",
+                       rows = "./data/out/europarl.row",
+                       cols = "./data/out/europarl.row",
                        format = "sm")
 
 #save the Space object in pickle format
