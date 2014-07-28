44ff23ed3b6e7e5be2d28e447beccf112c2bf1a2
Rewuff <imeyl@ya.ru>
2014-07-28 15:25:04 +0200

    reto path settings

--- parameters.py
+++ parameters.py
@@ -6,8 +6,8 @@ from os import sep
 # This variable has to be changed accordingly to a path where the
 # TreeTagger shell scripts reside. Also an absolute path can be set
 # here.
-TREETAGGER_PATH = "treetagger/cmd/"
-TREETAGGER_BASE_PATH = "treetagger/"
+TREETAGGER_PATH = "/home/reto/bin/cmd/"
+TREETAGGER_BASE_PATH = "/home/reto/bin/"
 
 
 ### default parameters used in create_input_data.py 
