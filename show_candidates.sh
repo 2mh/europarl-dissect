9c76bfaead2fd15ea30ef4bf71cde9b25fc707d6
Hernani Marques Madeira <hernani.marquesmadeira@uzh.ch>
2014-07-28 06:49:12 +0200

    Require one language to be "en".

--- show_candidates.sh
+++ show_candidates.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-if [ -z $1 ] || [ -z $2 ] || [ $1 = $2 ];
-then
+REQUIRED_LANG="en"
+
+function help {
 	echo "How to use     : $0 <langCode1> <langCode2> [<further arguments>]"
 	echo "Practice ex 1: : $0 de en"
 	echo "Practice ex 2: : $0 de en -nsp # No printing of stop words"
-else 
+
+}
+
+# Check for languages unset or they being the same.
+if [ -z $1 ] || [ -z $2 ] || [ $1 = $2 ]
+then
+	help
+# Require one language to be fixed.
+# XXX: Later it'll be possible to only specify one language for
+# semantic analysis in one language only.
+elif [ $1 != $REQUIRED_LANG ] && [ $2 != $REQUIRED_LANG ]
+then
+	echo "Remark: One of the two languages needs to be \"en\"."
+	help
+else
 	./besttranslations.py -s $1 -t $2 -i ./data/eval-${1}.txt ${@:3}
 fi
