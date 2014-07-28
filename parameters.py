a0136c2187b990ffa6c1d3ce05773eeebbc9e737
Hernani Marques Madeira <hernani.marquesmadeira@uzh.ch>
2014-07-28 19:07:42 +0200

    Add 100k files.

--- parameters.py
+++ parameters.py
@@ -6,7 +6,7 @@ from os import sep
 # This variable has to be changed accordingly to a path where the
 # TreeTagger shell scripts reside. Also an absolute path can be set
 # here.
-TREETAGGER_BASE_PATH = "/home/reto/bin/"
+TREETAGGER_BASE_PATH = "treetagger/"
 
 ### default parameters used in create_input_data.py 
 
