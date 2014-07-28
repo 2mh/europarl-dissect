5646426c62e658382663bd1e270f46fd932c18fd
Hernani Marques Madeira <hernani.marquesmadeira@uzh.ch>
2014-07-28 23:23:48 +0200

    Make counts.sh print ratio

--- eval/de_en/count.sh
+++ eval/de_en/count.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-for i in * ; do wc -l $i; grep %Y $i | wc -l ; echo "" ; done
+for i in *
+do 
+	echo "Filename:" $i
+	no_lines=$(cat $i | wc -l)
+	no_correct=$(cat $i | grep '%Y' | wc -l)
+	echo "Number of lines       : " $no_lines
+	echo "Number of correct ones: " $no_correct
+	echo -n "Ratio of correct ones : "
+	echo $no_correct $no_lines | awk '{ print $1/$2*100}'
+done
