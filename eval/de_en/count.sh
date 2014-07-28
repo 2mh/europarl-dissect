6ac3f1f1db5f6b740826b28d7cd602ba6b057512
Hernani Marques Madeira <hernani.marquesmadeira@uzh.ch>
2014-07-28 23:28:55 +0200

    More changes

--- eval/de_en/count.sh
+++ eval/de_en/count.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-for i in *
+
+function print_line
+{
+
+	echo "--------------------------------------------------------"
+}
+
+for i in $(ls *k-* | sort -g)
 do 
+	print_line
 	echo "Filename:" $i
 	no_lines=$(cat $i | wc -l)
 	no_correct=$(cat $i | grep '%Y' | wc -l)
-	echo "Number of lines       : " $no_lines
-	echo "Number of correct ones: " $no_correct
+	echo "Number of lines          : " $no_lines
+	echo "Number of correct ones   : " $no_correct
 	echo -n "Ratio of correct ones : "
 	echo $no_correct $no_lines | awk '{ print $1/$2*100}'
 done
+
+print_line
