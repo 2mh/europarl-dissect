7a09abaa8f574bf2a345099c15ff3fff18cbc416
Hernani Marques Madeira <hernani.marquesmadeira@uzh.ch>
2014-07-29 02:40:35 +0200

    Fix view

--- eval/de_en/count.sh
+++ eval/de_en/count.sh
@@ -12,8 +12,8 @@ do
 	echo "Filename:" $i
 	no_lines=$(cat $i | wc -l)
 	no_correct=$(cat $i | grep '%Y' | wc -l)
-	echo "Number of lines          : " $no_lines
-	echo "Number of correct ones   : " $no_correct
+	echo "Number of lines            : " $no_lines
+	echo "Number of correct ones     : " $no_correct
 	echo -n "Percentage of correct ones : "
 	echo $no_correct $no_lines | awk '{ print $1/$2*100 " %"}' 
 done
